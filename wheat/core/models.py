e937d97fa2cd5a4f2110e9aa911eca97d834145c
Sanskar Singh <sanskarsingh0504@gmail.com>
2026-03-01 04:27:38 -0700

    Add stable UUID serial to Author model

--- wheat/core/models.py
+++ wheat/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.db.models import Q
+import uuid
 
 # Core is only responsible for base offline functionality, other models for node and interconnectivity should be in a new app
 # -Z
@@ -13,22 +14,28 @@ VISIBILITIES = [
 ]
 
 class Author(models.Model):
-    url = models.URLField(unique=True)
+    # Stable identity that does NOT depend on DB pk
+    serial = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
 
+    url = models.URLField(unique=True)
     host = models.URLField()
-    displayName = models.CharField(max_length=255)    
+    displayName = models.CharField(max_length=255)
     github = models.URLField()
     description = models.TextField(blank=True, default="")
     profileImage = models.URLField()
     web = models.URLField()
+
     def get_followers(self):
         return Author.objects.filter(following__target=self)
-    
+
     def get_following(self):
         return Author.objects.filter(followers__actor=self)
 
     def get_friends(self):
         return Author.objects.filter(following__target=self, followers__actor=self)
+
+    def __str__(self):
+        return self.displayName
 
 
 
