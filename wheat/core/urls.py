a7d26b73113852e285e2890dba92bb64a4b6c8cd
Sanskar Singh <sanskarsingh0504@gmail.com>
2026-03-01 04:58:00 -0700

    Checkpoint before switching author URLs to serial

--- wheat/core/urls.py
+++ wheat/core/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("authors/<int:author_id>/", views.author_profile, name="author_profile"),
-    path("authors/<int:author_id>/edit/", views.author_edit, name="author_edit"),
+
+    # stable Author.serial (UUID) in URLs
+    path("authors/<uuid:author_serial>/", views.author_profile, name="author_profile"),
+    path("authors/<uuid:author_serial>/edit/", views.author_edit, name="author_edit"),
+
     path("authors/", views.author_list, name="author_list"),
 ]
