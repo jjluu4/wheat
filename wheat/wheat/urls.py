16e7ecd5b2ae9c35e3e88c984d85503159997b97
dvold <eyedoooit@gmail.com>
2026-03-02 10:31:19 -0700

    added DRF-YASG app for documentation

--- wheat/wheat/urls.py
+++ wheat/wheat/urls.py
@@ -17,10 +17,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Wheat API",
+      default_version='v1',
+      description="API used for wheat's social distribution app",
+      terms_of_service="",
+      contact=openapi.Contact(email="None"),
+      license=openapi.License(name="None"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("accounts/", include("django.contrib.auth.urls")),
 
     path("", include("core.urls")),
+    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),    
 ]
