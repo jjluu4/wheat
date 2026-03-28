bf275471c4225b0f9b0496106630ea4f0906bc65
dvold <eyedoooit@gmail.com>
2026-03-28 16:12:19 -0600

    small test change

--- wheat/core/tests/tests.py
+++ wheat/core/tests/tests.py
@@ -500,7 +500,7 @@ class EntryDeleteTests(TestCase):
             author=self.author,
             content="Remote",
             content_type="text/plain",
-            visibility="FRIENDS",
+            visibility="UNLISTED",
             published=timezone.now(),
         )  
 
