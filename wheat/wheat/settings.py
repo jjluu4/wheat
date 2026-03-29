c7bda4c9b16d6681c39b46fae0ae09abccdda2a5
Zumzu <zanegcrawford@gmail.com>
2026-03-28 23:40:14 -0600

    Disable debug

--- wheat/wheat/settings.py
+++ wheat/wheat/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))ro_$@8x=vvhim!hx*-n_ti9cx#^qs-+()t%ch2k#$v=_1j#&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
