0175b021e9f56f68e3525ffe65c682edce9dd366
jjluu4 <jjluu@ualberta.ca>
2026-03-30 13:28:45 -0600

    corrected timezone and disabled debug for production

--- wheat/wheat/settings.py
+++ wheat/wheat/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))ro_$@8x=vvhim!hx*-n_ti9cx#^qs-+()t%ch2k#$v=_1j#&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Edmonton'
 
 USE_I18N = True
 
